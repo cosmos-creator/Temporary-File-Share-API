ee3f2aaf4b3eb3a11f6c135a4fc3f132eb6d9beb
Ez <ezy.misc@gmail.com>
2026-08-11 15:32:01 +0530

    index code to fasten up lookups, since lookups are code based not id based

--- app/models/file.py
+++ app/models/file.py
@@ -5,5 +5,5 @@ from app.db import Base
 class UploadedFile(Base):
     __tablename__ = "files"
     id: Mapped[int] = mapped_column(primary_key=True)
-    short_code: Mapped[str] = mapped_column(String(12), unique=True)
+    short_code: Mapped[str] = mapped_column(String(12), unique=True, index=True)
     original_filename: Mapped[str] = mapped_column(String(255))
