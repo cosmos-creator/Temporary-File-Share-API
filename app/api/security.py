8d21a4371b9a94946a23ad81e2b256ddad21112e
Ez <ezy.misc@gmail.com>
2026-08-26 18:12:56 +0530

    Auth: created dependency function to get current user

--- app/api/security.py
+++ app/api/security.py
@@ -2,14 +2,38 @@ import os
 from datetime import UTC, datetime, timedelta
 
 from dotenv import load_dotenv
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.db import engine
+from app.models.user import User
 
 load_dotenv()
 
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 pwd_context = CryptContext(schemes=["brypt"], depricated="auto")
+
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+def get_current_user(token: str = Depends(oauth_scheme), db: Session = Depends(get_db)):
+    username = verify_jwt(token)
+    if username is None:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    user = db.execute(select(User).where(User.username == username)).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    return user
 
 def hash(password: str) -> str:
     return pwd_context.hash(password)
