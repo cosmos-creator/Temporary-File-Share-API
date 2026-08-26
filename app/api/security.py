6bc3cab9e9b06fca09c50f08872af16a72057bdb
Ez <ezy.misc@gmail.com>
2026-08-26 17:53:43 +0530

    Auth: created jwt creation and verification functions

--- app/api/security.py
+++ app/api/security.py
@@ -1,4 +1,13 @@
+import os
+from datetime import UTC, datetime, timedelta
+
+from dotenv import load_dotenv
+from jose import JWTError, jwt
 from passlib.context import CryptContext
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 pwd_context = CryptContext(schemes=["brypt"], depricated="auto")
 
@@ -6,4 +15,17 @@ def hash(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify(password: str, hash: str) -> bool:
-     return pwd_context.verify(password, hash)
+    return pwd_context.verify(password, hash)
+
+def create_jwt(data: dict, expires_at: timedelta = timedelta(minutes=15)):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + expires_at
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, "HS256")
+
+def verify_jwt(token: str) -> str | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["SH256"])
+        return payload.get("sub")
+    except JWTError:
+        return None
